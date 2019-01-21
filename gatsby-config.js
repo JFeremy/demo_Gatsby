527f8f26c4263364c80896913a96861bb29b2eae
FUMERON Jérémy <jeremy.fumeron@orange.com>
2019-01-21 10:02:24 +0100

    Mise à jour de l'utilisation de graphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,17 @@ module.exports = {
         options: {
           pathToConfigModule: 'src/utils/typography.js'
         }
+      },
+      {
+        resolve: "gatsby-source-graphql",
+        options: {
+          // This type will contain remote schema Query type
+          typeName: "DEMOCITYV4",
+          // This is the field under which it's accessible
+          fieldName: "democityv4",
+          // URL to query from
+          url: "https://demo-city-v4-itg.smartappscenter.com/contentdelivery/graphql",
+        },
       }
     ]
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,61 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Layout from "../components/layout"
 import Grid from '@material-ui/core/Grid';
+import { graphql } from "gatsby"
 
-import Data from "../components/data";
+//import Data from "../components/data";
 import Poi from "../components/poi";
 
+const App = ({ data }) => (
+    <Layout>    
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+        >
+            <h1>Vous avez {data.democityv4.pois.edges.length}  points d'intérêts proche de vous !</h1>
+        </Grid>
+        <Grid 
+            container 
+            direction="row"
+            spacing={16}
+            justify="center"
+            alignItems="stretch"
+        >
+            {data.democityv4.pois.edges.map(dataPoi => <Poi key={dataPoi.node.id} dataPoi={dataPoi.node}/>)}
+        </Grid>
+    </Layout>
+);
 
-class App extends Component {
-    state = {
-        dataPois: []
+export const query = graphql`
+    query {
+        democityv4 {
+            pois {
+                edges {
+                    node { 
+                        id
+                        title
+                        categories {
+                            edges {
+                                node {
+                                    title
+                                }
+                            }
+                        }
+                        description        
+                        location {
+                            address
+                            zipCode
+                            city
+                        }
+                        websites
+                    }
+                }
+            }
+        }        
     }
-
-    async componentDidMount() {
-        this.setState({dataPois: await Data()})       
-    }
-
-    render() {
-        return (
-            <Layout>    
-                <Grid
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                >
-                    <h1>Vous avez {this.state.dataPois.length}  points d'intérêts proche de vous !</h1>
-                </Grid>
-                <Grid 
-                    container 
-                    direction="row"
-                    spacing={16}
-                    justify="center"
-                    alignItems="stretch"
-                >
-                    {this.state.dataPois.map(dataPoi => <Poi key={dataPoi.node.id} dataPoi={dataPoi.node}/>)}
-                </Grid>
-        </Layout>
-        );
-    }
-}
-
-
+`;
 
 
 export default App;
